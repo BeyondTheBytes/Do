17f24dd73bf01fb4eaa068f61f13765c78045d4e
Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>
2022-08-14 03:24:17 -0300

    :bug: FIX: Fix iOS build script
    Signed-off-by: Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>

--- scripts/build-ios.sh
+++ scripts/build-ios.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-./scripts/build.sh prod ios $1 &&
+./scripts/build.sh ios $1 &&
 echo "" &&
 echo "Run the following to upload: " &&
-echo "xcrun altool --upload-app --type ios -f build/ios/ipa/legumo.ipa --apiKey KF8UP9GAN9 --apiIssuer ac246c3d-529d-4eee-ac19-0a669b27790e" &&
+echo "xcrun altool --upload-app --type ios -f build/ios/ipa/Do..ipa --apiKey KF8UP9GAN9 --apiIssuer ac246c3d-529d-4eee-ac19-0a669b27790e" &&
 echo "" &&
 echo "After that, do the following:" &&
 echo "1. Accept the ipa: https://appstoreconnect.apple.com/apps/1589305379/testflight/ios" &&
