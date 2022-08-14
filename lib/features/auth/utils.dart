79be649dcd9cf28cf463d3df83e0ecf878c979b5
Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>
2022-08-14 01:17:55 -0300

    :sparkles: FEATURE: Create UserProvider
    Signed-off-by: Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>

--- lib/features/auth/utils.dart
+++ lib/features/auth/utils.dart
@@ -1,5 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+
+import '../../config/state.dart';
+
+class UserProvider extends StatelessWidget {
+  final Widget Function(BuildContext, User) builder;
+  const UserProvider({required this.builder});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: AppState.auth.userStream,
+      initialData: AppState.auth.currentUser,
+      builder: (context, snapshot) => builder(context, snapshot.data!),
+    );
+  }
+}
+
+// FORMATER
 
 class CustomInputFormatter {
   static const phoneLength = 15;
