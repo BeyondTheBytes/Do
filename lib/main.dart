2e28889aaf118916d3de9f79e4a3032df8f59fa6
Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>
2023-01-24 00:02:30 -0300

    :bug: FIX: Only mocked dependencies in this branch
    Signed-off-by: Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
@@ -28,15 +29,7 @@ class AppDependencies {
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await prepareApp(
-    AppDependencies(
-      firebaseAuth: FirebaseAuth.instance,
-      firebaseFirestore: FirebaseFirestore.instance,
-      firebaseStorage: FirebaseStorage.instance,
-    ),
-  );
-
+  await prepareApp();
   runApp(App());
 }
 
